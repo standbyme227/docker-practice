a7c44c295cf0451804090f5e3d60120cc887500a
ShinJongMin <standbyme227@gmail.com>
2018-03-13 08:57:15 +0900

    members의 user모델에 img_profile field 추가

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-secrets = json.loads(open(SECRETS_DEV, 'rt').read())
+secrets = json.loads(open(SECRETS_PRODUCTION, 'rt').read())
 
 ALLOWED_HOSTS = []
 WSGI_APPLICATION = 'config.wsgi.production.application'
@@ -10,3 +10,7 @@ WSGI_APPLICATION = 'config.wsgi.production.application'
 INSTALLED_APPS += [
     'django_extensions',
 ]
+
+
+DEFAULT_FILE_STORAGE = 'config.storage.DeFaultFilesStorage'
+STATICFILES_STORAGE = 'config.storage.StaticFilesStorage'
